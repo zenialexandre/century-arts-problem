c72cd4c11eb0ce168293848256c7d9955535f020
zenialexandre <zenialexandre123@gmail.com>
2024-03-12 02:20:59 -0300

    feat: finishing the input process flow and starting the convex verification

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def initialize_program() -> None:
+    arts_counter = 0
+    art_gallery_dict = {}
     corner_points_number = int(input())
 
     while (
@@ -6,27 +8,56 @@ def initialize_program() -> None:
         and corner_points_number >= 3 \
         and corner_points_number <= 50
     ):
-        art_gallery_dict = {}
+        arts_counter = arts_counter + 1
 
-        for corner_points_number_row in range(corner_points_number):
-            populate_art_gallery(art_gallery_dict, corner_points_number_row)
+        for _ in range(corner_points_number):
+            populate_art_gallery(art_gallery_dict, arts_counter)
 
         corner_points_number = int(input())
 
-    print(art_gallery_dict)
+    validateConvexPolygons(art_gallery_dict)
 
-def populate_art_gallery(art_gallery_dict, corner_points_number_row) -> dict:
-    art_gallery_dict_key = f'art_{corner_points_number_row}'
+def populate_art_gallery(art_gallery_dict, arts_counter) -> None:
+    art_gallery_dict_key = f'art_{arts_counter}'
     x_coordinate = int(input())
     y_coordinate = int(input())
+
+    if (art_gallery_dict_key not in art_gallery_dict):
+        art_gallery_dict[art_gallery_dict_key] = []
 
     while (
         (x_coordinate >= 0 and x_coordinate <= 1000) \
         and (y_coordinate >= 0 and y_coordinate <= 1000)
     ):
-        art_gallery_dict[art_gallery_dict_key] = [x_coordinate, y_coordinate]
+        art_gallery_dict[art_gallery_dict_key].append([x_coordinate, y_coordinate])
         break
 
-    return art_gallery_dict
+def validateConvexPolygons(art_gallery_dict) -> None:
+    print('\n')
+    vectorial_product = 0
+
+    for _, art_gallery_dict_points_array in art_gallery_dict.items():
+        vectorial_product = get_vectorial_product_calculated(art_gallery_dict_points_array)
+
+        if (vectorial_product > 0.000001):
+            print('No\n')
+        elif (vectorial_product < 0.000001):
+            print('Yes\n')
+
+def get_vectorial_product_calculated(art_gallery_dict_points_array) -> float:
+    vector_first_second_points = [
+        art_gallery_dict_points_array[1][0] - art_gallery_dict_points_array[0][0],
+        art_gallery_dict_points_array[1][1] - art_gallery_dict_points_array[0][1]
+    ]
+
+    vector_first_third_points = [
+        art_gallery_dict_points_array[2][0] - art_gallery_dict_points_array[0][0],
+        art_gallery_dict_points_array[2][1] - art_gallery_dict_points_array[0][1]
+    ]
+
+    return abs(
+        vector_first_second_points[0] * vector_first_third_points[1] \
+            - vector_first_second_points[1] * vector_first_third_points[0]
+    )
 
 initialize_program()
